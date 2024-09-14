d03f41b2cf951be8f8ea6163c5f1a38c62fc928d
seedhe-maut-sm <nishantsinghania86@gmail.com>
2024-09-14 08:24:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,82 @@
+import os, time
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+   
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","")     
+    DB_URL  = os.environ.get("DB_URL","")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/f8138ab9d9382104ab910.jpg")
+    ADMIN = int(os.environ.get("ADMIN", ""))
+
+    # channels logs
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """Join all channel and clam upi cash"""
+
+    ABOUT_TXT = """
+╭───────────────⍟
+├<b>🤖 My Name</b> : {}
+├<b>🖥️ Developer</b> : <a href=link>maut</a> 
+├<b>👨‍💻 Programer</b> : <a href=https://t.me/Rohesh_Gavit>Rohesh Developer</a>
+├<b>📕 Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
+├<b>✏️ Language</b> : <a href=https://www.python.org>Python 3</a>
+├<b>💾 Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>
+├<b>📊 Build Version</b> : <a href=https://t.me/seedhe_maut>Rename v4.5.0</a></b>     
+╰───────────────⍟
+"""
+
+    HELP_TXT = """
+🌌 <b><u>How To Set Thumbnail</u></b>
+  
+➪ /start - Start The Bot And Send Any Photo To Automatically Set Thumbnail.
+➪ /del_thumb - Use This Command To Delete Your Old Thumbnail.
+➪ /view_thumb - Use This Command To View Your Current Thumbnail.
+
+📑 <b><u>How To Set Custom Caption</u></b>
+
+➪ /set_caption - Use This Command To Set A Custom Caption
+➪ /see_caption - Use This Command To View Your Custom Caption
+➪ /del_caption - Use This Command To Delete Your Custom Caption
+➪ Example - <code>/set_caption 📕 Name ➠ : {filename}
+
+🔗 Size ➠ : {filesize} 
+
+⏰ Duration ➠ : {duration}</code>
+
+✏️ <b><u>How To Rename A File</u></b>
+
+➪ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].           
+
+𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/seedhe_maut>Developer</a>
+"""
+
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ <b>🗃️ Sɪᴢᴇ:</b> {1} | {2}
+┣⪼ <b>⏳️ Dᴏɴᴇ :</b> {0}%
+┣⪼ <b>🚀 Sᴩᴇᴇᴅ:</b> {3}/s
+┣⪼ <b>⏰️ Eᴛᴀ:</b> {4}
+╰━━━━━━━━━━━━━━━➣"""
+
+    DONATE_TXT = """
+<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+
+<b>🛍 UPI ID:</b> `7507446728upi@axl`
+"""
